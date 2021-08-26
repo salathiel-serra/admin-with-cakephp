577079ec626416fd0a60e3c900a1862b526b2434
thiel.serra@gmail.com <thiel.serra@gmail.com>
2021-08-25 22:39:52 -0300

    Adicionando class Behavior para isolar lógica de upload de imagens

---
+++ src/Model/Behavior/UploadBehavior.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Model\Behavior;
+
+use Cake\ORM\Behavior;
+use Cake\ORM\Table;
+
+/**
+ * Upload behavior
+ */
+class UploadBehavior extends Behavior
+{
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+
+    public function singleUpload(array $file, $path)
+    {
+        return $this->upload($file, $path);
+    }
+
+    public function upload($file, $path)
+    {
+        extract($file);
+        $name = $this->slug($name);
+
+        if (move_uploaded_file($tmp_name, $path.$name)) { 
+            return $name;
+        } else {
+            return false;
+        } 
+    }
+
+    public function slug($name)
+    {
+        $formato['a'] = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]/?;:,\\\'<>°ºª';
+        $formato['b'] = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr                                ';
+        $name = strtr(utf8_decode($name), utf8_decode($formato['a']), $formato['b']);
+        $name = str_replace(' ', '-', $name);
+
+        $name = str_replace(['-----', '----', '---', '--'], '-', $name);
+
+        return strtolower($name);
+    }
+}
